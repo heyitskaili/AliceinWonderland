72e9d061e45c962e9138ade8979b574783dda478
heyitskaili <63416127+heyitskaili@users.noreply.github.com>
2020-10-10 22:11:30 -0400

    Finished Updating

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,27 +5,27 @@ import {section4Animation} from "./sections/section4.js"
 import {section5Animation} from "./sections/section5.js"
 import {section8Animation} from "./sections/section8.js"
 
-// var $burger = $("#burger-icon");
-// var $nav = $('nav[data-nav]');
-// var $navA = $('nav[data-nav] a');
-// var isVisible = false;
+var $burger = $("#burger-icon");
+var $nav = $('nav[data-nav]');
+var $navA = $('nav[data-nav] a');
+var isVisible = false;
 
-// function mouseClick(){
+function mouseClick(){
 
-//     console.log("click");
-//     if(isVisible === false){
-//         $nav.show();
-//         isVisible = true;
-//     }
-//     else{
-//         $nav.hide();
-//         isVisible = false;
-//     }
-// }
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+}
 
-// $burger.on( "click", mouseClick);
-// // close the mobile menu when menu link is clicked
-// $navA.on( "click", mouseClick);
+$burger.on( "click", mouseClick);
+// close the mobile menu when menu link is clicked
+$navA.on( "click", mouseClick);
 
 
 section1Animation();
